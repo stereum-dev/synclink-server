f2822a76c3efb786aa011e9450b378a58c94c570
Ali Montajebi <montajebi.ali@gmail.com>
2022-10-04 11:58:02 +0330

    ADD: eth address to config

--- config/config.py
+++ config/config.py
@@ -1,14 +1,17 @@
+import string
 import yaml
 from pydantic import BaseModel
 
 
 class AppConfig(BaseModel):
     port: int
+    eth_api_address: str
 
 
 def read(file_name):
     defaultConfig: AppConfig = {
-        "port": 8000
+        "port": 8000,
+        "eth_api_address": "http://localhost:5051",
     }
 
     with open(file_name, "r") as f:
