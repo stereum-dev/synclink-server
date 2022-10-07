17087d20c400af0c99df3d82d50c2a0eb79c6425
daverolo <107847185+daverolo@users.noreply.github.com>
2022-10-07 12:26:23 +0200

    REFACTOR: use config address for server startup

--- main.py
+++ main.py
@@ -17,5 +17,5 @@ app.include_router(eth_router, prefix='/eth')
 
 if __name__ == "__main__":
     config = read('config.yaml')
-
-    uvicorn.run("main:app", host="0.0.0.0", port=config['port'], reload=True)
+    
+    uvicorn.run("main:app", host=config['addr'], port=config['port'], reload=True)
