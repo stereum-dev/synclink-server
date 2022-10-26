3648ece22b7077163cc4a45828efe9685bd78b8b
Ali Montajebi <montajebi.ali@gmail.com>
2022-10-26 23:08:42 +0330

    ADD: logger

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-import uvicorn
-from fastapi import BackgroundTasks, FastAPI
-from fastapi.responses import StreamingResponse
-import httpx
-
+from fastapi import FastAPI
+from loguru import logger
+from uvicorn import Config, Server
 
 from config.config import read
+from config.logger import setup_logging
 from routes.eth import eth_router
-
-client = httpx.AsyncClient()
-
 
 app = FastAPI(
     title="SyncLink Server API",
@@ -22,11 +18,15 @@ app.include_router(eth_router, prefix='/eth')
 if __name__ == "__main__":
     config = read('config.yaml')
 
+    server = Server(Config("main:app", host=config['addr'],
+                           port=config['port'], reload=True))
+
+    setup_logging()
+
+    server.run()
+
     docs_addr = config['addr'] if config['addr'] != "0.0.0.0" else "127.0.0.1"
     docs_port = config['port']
-    docs_line = '=' * 75
-    print(docs_line);
-    print(f"Server starting, find API docs at http://{docs_addr}:{docs_port}/docs")
-    print(docs_line);
-    
-    uvicorn.run("main:app", host=config['addr'], port=config['port'], reload=True)
+
+    logger.info(
+        f"Server starting, find API docs at http://{docs_addr}:{docs_port}/docs")
