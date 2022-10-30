55987a211951cd8e1c163bf6244e7bce4cfc0820
Ali Montajebi <montajebi.ali@gmail.com>
2022-10-30 09:11:17 +0330

    FIX: logger config

--- main.py
+++ main.py
@@ -3,15 +3,19 @@ from loguru import logger
 import uvicorn
 
 from config.config import read
+from config.logger import LOG_LEVEL, UVICORN_LOGGING_CONFIG, setup_logging
 from routes.eth import eth_router
 from routes.synclink import synclink_router
 import core.synclink
+
 
 app = FastAPI(
     title="SyncLink Server API",
     description="Specification of the SyncLink Server API",
     version="0.1.0",
 )
+
+setup_logging(LOG_LEVEL, False)
 
 app.include_router(synclink_router, prefix='/synclink')
 app.include_router(eth_router, prefix='/eth')
@@ -32,4 +36,4 @@ if __name__ == "__main__":
         f"Server starting, find API docs at http://{docs_addr}:{docs_port}/docs")
 
     uvicorn.run("main:app", host=config.addr,
-                port=config.port, reload=True)
+                port=config.port, reload=True, log_config=UVICORN_LOGGING_CONFIG)
